3947d7d3f2de811556eaaee74c2f0df991398350
Chisomo Sakala <chisomo@makombo.org>
2016-02-20 23:48:36 -0500

    done with part 3 urlconfs, views ans templates

--- polls/admin.py
+++ polls/admin.py
@@ -5,4 +5,4 @@ from .models import Question
 from .models import Choice
 
 admin.site.register(Question)
-admin.site.register(Choice)
+#admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,24 @@
-# Copyright 2015 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from doctest import _OutputRedirectingPdb
+from .models import Question
+
+
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+def index(request):
+    return render(request, 'polls/index.html', {'latest_question_list': Question.objects.order_by('-pub_date')[:5]})
+
+
+def detail(request, question_id):
+    return render(request, 'polls/detail.html', {'question': get_object_or_404(Question, pk=question_id)})
+
+
+
+
 
 from django.http import HttpResponse
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
 
-
-def index(request):
-    return HttpResponse("Hello, world. This is The Test Polls App For Django on Google App Engin By Somcho.")
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
