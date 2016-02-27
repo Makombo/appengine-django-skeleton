fd18ee5570cef191f50bc8c9536919e6cb9ec57f
Makombo <makombosakala@gmail.com>
2016-02-27 01:03:08 -0500

    fixed pub_date == None
    caused by not setting the exact version number of MySQLdb to  1.2.5 in
    app.yml

--- polls/models.py
+++ polls/models.py
@@ -16,8 +16,9 @@ class Question(models.Model):
 
 	#why is self.pub_date on the appengine depserver but not the devserver?
     def was_published_recently(self):
-        return True self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    was_published_recently.admin_order_field = 'pub_date'
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=30)
+    
+	was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
